ab8a6c7b9f44e07d1c3e2244acb34655e80e449d
Ruth <ruth.in5667@gmail.com>
2024-01-02 10:25:35 +0200

    from app

--- src/components/dataStore/mobx.js
+++ src/components/dataStore/mobx.js
@@ -1,7 +1,15 @@
 import { observable, action, computed, makeObservable, runInAction } from 'mobx';
 class MobxData {
     isLogin = false;
-    buisnessData = {};
+    buisnessData = {
+        name: "Coding Academy",
+        address: "Rothschild 60 Tel Aviv",
+        phone: "03-1234567",
+        owner: "Yariv Katz",
+        logo: "https://media.istockphoto.com/id/1460985803/photo/hand-flipping-of-2023-to-2024-on-wooden-block-cube-for-preparation-new-year-change-and-start.jpg?s=612x612&w=0&k=20&c=29THUBIis7vYKYNIKEyms6xDXbJvIHYs3litHohd5xQ=",
+        description: "A successful BH ",
+
+    };
     constructor() {
         makeObservable(this, {
             serviceType: observable,
@@ -32,7 +40,7 @@ class MobxData {
         this.buisnessData = val;
     }
     initBuisnessData = async () => {
-        let data = await fetch('http://localhost:8787/businessData')
+        let data = await fetch('http://localhost:8787/businessData');
         let datajson = await data.json();
         this.buisnessData = datajson;
 
